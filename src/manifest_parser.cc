d91e411654ea1e4c2a5dbc566c5a647f12104d59
Nico Weber <nicolasweber@gmx.de>
2013-07-07 11:49:28 -0700

    lexer support for scopes
    I was worried that the additional branches would slow down lexing of
    files, but that seems to not be the case (tested with null builds of
    the 'content' target in chromium; I couldn't see a difference with and
    without this patch).

--- src/manifest_parser.cc
+++ src/manifest_parser.cc
@@ -98,6 +98,14 @@ bool ManifestParser::Parse(const string& filename, const string& input,
       if (!ParseFileInclude(true, err))
         return false;
       break;
+    case Lexer::SCOPE:
+      if (!ParseScope(err))
+        return false;
+      break;
+    case Lexer::ENDSCOPE:
+      if (!ParseEndScope(err))
+        return false;
+      break;
     case Lexer::ERROR: {
       return lexer_.Error(lexer_.DescribeLastError(), err);
     }
@@ -410,6 +418,20 @@ bool ManifestParser::ParseFileInclude(bool new_scope, string* err) {
   if (!subparser.Load(path, err, &lexer_))
     return false;
 
+  if (!ExpectToken(Lexer::NEWLINE, err))
+    return false;
+
+  return true;
+}
+
+bool ManifestParser::ParseScope(string* err) {
+  if (!ExpectToken(Lexer::NEWLINE, err))
+    return false;
+
+  return true;
+}
+
+bool ManifestParser::ParseEndScope(string* err) {
   if (!ExpectToken(Lexer::NEWLINE, err))
     return false;
 
